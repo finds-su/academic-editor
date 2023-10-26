e9fbdfbaffb92f48b3ac93ec8344ebcb5eff048c
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2023-10-27 00:05:13 +0300

    feat: formatting, add react router

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,17 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import React, { useState } from 'react';
+import IndexLayout from '@/components/pages/index/index-layout.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1>Welcome to Tauri!</h1>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<IndexLayout />} />
+                {/*<Route path="/blog/*" element={<BlogApp />} />*/}
+                {/*<Route path="/users/*" element={<UserApp />} />*/}
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
